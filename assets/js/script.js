8353daa3d17be7e2c12a98c41c98371a22945acd
FlorentViel <43536189+FlorentViel@users.noreply.github.com>
2018-10-16 09:15:23 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// On écoute le click sur les éléments qui ont la classe .nav-item
+$('.nav-item').click(function (event) {
+    // console.log(event);
+    // event.currentTarget == this
+    $('.nav-item').removeClass('active');
+    $(this).addClass('active');
+});
+
+var word = $('#tap-text').text().trim();
+
+$('#tap-text').text('');
+
+// Parcourir les lettres du mot  
+
+for (let i = 0; i < word.length; i++){
+    //console.log(word[i]);
+
+setTimeout(function(){
+    console.log(word[i]);
+
+    $('#tap-text').append(word[i]);
+}, 300 * i);
+}
+
